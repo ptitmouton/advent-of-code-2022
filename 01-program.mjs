d27f9c3f1c25baca5091f182ff66f59222093e92
Alexis Rinaldoni <alexis.rinaldoni@be-terna.com>
2022-12-11 10:21:09 +0100

    day 1

---
+++ 01-program.mjs
@@ -0,0 +1,32 @@
+import {readFile} from 'fs/promises';
+
+(async () => {
+    const input = await readFile('01-input.txt', 'utf8');
+
+    const elfes = input.split(/\n\r?\n\r?/);
+
+    const elfTotals = elfes.map(elf => {
+      return elf.split(/\n\r?/).reduce((acc, current) => {
+        return acc + Number(current);
+      }, 0);
+    });
+
+  elfTotals.sort((a, b) => b - a);
+
+  const max = elfTotals[0];
+
+  console.log(`The elf carrying the most calories carries ${max} calories`);
+
+  const maxThree = elfTotals.slice(0, 3);
+
+  const total = maxThree.reduce((acc, current) => {
+    return acc + current;
+  }, 0);
+
+  console.log(`The total calories carried by the three strongest elves is ${total} calories`);
+
+
+
+})();
+
+
